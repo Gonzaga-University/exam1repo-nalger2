9ccfdf7a14c2d81663821d6dcee281d30ed093b3
nalger2 <nalger2@zagmail.gonzaga.edu>
2020-10-12 23:43:06 -0700

    final commit 3

--- src/main.py
+++ src/main.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":
     print("Done with program")
     print("Hello this is Nelly!")
     print("I am demonstrating my ability to manage a git repo!")
+    print("this is my final commit. goodbye :)")
